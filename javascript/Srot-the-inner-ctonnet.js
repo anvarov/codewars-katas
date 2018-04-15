1886dc5e7e69ae60a6922e7b157124d6580d1446
anvarov <akmalanvaroff@gmail.com>
2018-04-15 14:52:53 -0500

    added solution for 'sort the inner content' challenge

---
+++ javascript/Srot-the-inner-ctonnet.js
@@ -0,0 +1,20 @@
+// // Instuctions:
+// #Srot the inner ctnnoet in dsnnieedcg oredr
+
+// You have to sort the inner content of every word of a string in descending order.
+// The inner content is the content of a word without first and the last char.
+// @flow
+
+function sortTheInnerContent(words) {
+  const arr = words.split(' ');
+  const newArr = arr.map((x) => {
+    if (x.length > 2) {
+      const strArr = x.split('');
+      const sliced = strArr.slice(1, x.length - 1);
+      return x[0] + sliced.sort().reverse().join('') + x[x.length - 1];
+    }
+    return x;
+  });
+  return newArr.join(' ');
+}
+sortTheInnerContent('this is a test string');
